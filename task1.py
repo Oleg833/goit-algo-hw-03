f99f2dddbd2be93af00315138147dc073a8d2a3d
Oleg833 <oleg.kovtun447@gmail.com>
2024-01-15 16:20:39 +0200

    add ignore

---
+++ task1.py
@@ -0,0 +1,41 @@
+import os
+import shutil
+import argparse
+
+def copy_files(src_dir, dest_dir):
+    if not os.path.exists(src_dir):
+        print(f"Помилка: Вихідна директорія {src_dir} не існує.")
+        return
+    
+    if not os.path.exists(dest_dir):
+        os.makedirs(dest_dir)
+
+
+    for root, dirs, files in os.walk(src_dir):
+        for file in files:
+            src_path = os.path.join(root, file)
+
+            try:
+                copy_file(src_path, dest_dir)
+            except Exception as e:
+                print(f"Помилка копіювання файлу {src_path}: {e}")
+
+def copy_file(src_path, dest_dir):
+    _, file_extension = os.path.splitext(src_path)
+
+    subdir = os.path.join(dest_dir, file_extension[1:])
+    if not os.path.exists(subdir):
+        os.makedirs(subdir)
+
+    shutil.copy2(src_path, os.path.join(subdir, os.path.basename(src_path)))
+
+def main():
+    parser = argparse.ArgumentParser(description="Рекурсивно копіює файли та сортує їх за розширенням.")
+    parser.add_argument("src_dir", help="Шлях до вихідної директорії")
+    parser.add_argument("dest_dir", nargs="?", default="dist", help="Шлях до директорії призначення")
+    args = parser.parse_args()
+
+    copy_files(args.src_dir, args.dest_dir)
+
+if __name__ == "__main__":
+    main()
---
+++ task2.py
@@ -0,0 +1,41 @@
+import turtle
+
+def koch_snowflake(t, order, size):
+    if order == 0:
+        t.forward(size)
+    else:
+        for angle in [60, -120, 60, 0]:
+            koch_snowflake(t, order - 1, size / 3)
+            t.left(angle)
+
+def draw_complete_snowflake(turtle, order, size):
+    for _ in range(3):
+        koch_snowflake(turtle, order, size)
+        turtle.right(120)
+
+def main():
+    try:
+        recursion_level = int(input("Введіть рівень рекурсії для сніжинки Коха: "))
+    except ValueError:
+        print("Будь ласка, введіть ціле число.")
+        return
+
+    window = turtle.Screen()
+    window.bgcolor("white")
+    window.title("Сніжинка Коха")
+
+    snowflake_turtle = turtle.Turtle()
+    snowflake_turtle.color("blue")
+    snowflake_turtle.width(2)
+    snowflake_turtle.speed(2)
+
+    snowflake_turtle.penup()
+    snowflake_turtle.goto(-150, 90)
+    snowflake_turtle.pendown()
+
+    draw_complete_snowflake(snowflake_turtle, recursion_level, 300)
+
+    window.exitonclick()
+
+if __name__ == "__main__":
+    main()
